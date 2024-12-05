25468b19e177ce55d980845e94bc57e4c4b6763f
ngPoi <ngpoi1810@gmail.com>
2024-12-05 15:45:25 +0700

    commit fast pizza website

---
+++ src/features/cart/DeleteItem.jsx
@@ -0,0 +1,12 @@
+import { useDispatch } from "react-redux";
+import Button from "../../ui/Button";
+import { delItem } from "./cartSlice";
+
+export default function DeleteItem({ pizzaId }) {
+  const dispatch = useDispatch();
+  return (
+    <Button type="small" onClick={() => dispatch(delItem(pizzaId))}>
+      Delete
+    </Button>
+  );
+}
